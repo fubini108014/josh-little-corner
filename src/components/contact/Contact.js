56259ae22a6bdc240e768e112f00abee1eacf4c4
LAPTOP-3V4UE3CM\user <fubini4817@gmail.com>
2022-06-23 00:51:09 +0800

    contact form css style

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -3,7 +3,11 @@ import MessageIcon from "@mui/icons-material/Message";
 import { styled } from "@mui/material/styles";
 import IconButton from "@mui/material/IconButton";
 import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
 import ContactForm from "./ContactForm";
+import CloseIcon from "@mui/icons-material/Close";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
 
 const ContactIcon = styled(IconButton)({
   position: "absolute",
@@ -29,7 +33,29 @@ function Contact() {
         <MessageIcon />
       </ContactIcon>
       <Dialog onClose={handleClose} open={open}>
-        <ContactForm />
+        <DialogTitle sx={{ m: 0, p: 2 }}>
+          與我聯繫
+          <IconButton
+            aria-label="close"
+            onClick={handleClose}
+            sx={{
+              position: "absolute",
+              right: 8,
+              top: 8,
+              color: (theme) => theme.palette.grey[500],
+            }}
+          >
+            <CloseIcon />
+          </IconButton>
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To subscribe to this website, please enter your email address here.
+            We will send updates occasionally.
+          </DialogContentText>
+
+          <ContactForm onClose={handleClose} />
+        </DialogContent>
       </Dialog>
     </>
   );
--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -1,29 +1,89 @@
 // Make sure to run npm install @formspree/react
 // For more help visit https://formspr.ee/react-help
-import React from "react";
-import { useForm, ValidationError } from "@formspree/react";
-function ContactForm() {
+import React, { useEffect } from "react";
+import { useForm } from "@formspree/react";
+import { styled } from "@mui/material/styles";
+import TextField from "@mui/material/TextField";
+import Swal from "sweetalert2";
+import Button from "@mui/material/Button";
+import EmailIcon from "@mui/icons-material/Email";
+import InputAdornment from "@mui/material/InputAdornment";
+
+const FormContainer = styled("div")({
+  width: "100%",
+  //height: "500px",
+});
+function ContactForm({ onClose = () => {} }) {
   const [state, handleSubmit] = useForm("meqnwqnd");
-  if (state.succeeded) {
-    return <p>Thanks for joining!</p>;
-  }
+
+  useEffect(() => {
+    if (state.succeeded) {
+      onClose();
+
+      Swal.fire({
+        title: "訊息發送成功",
+        text: "感謝您的聯繫，我會盡快回覆您!",
+        icon: "success",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    }
+  }, [state.succeeded]);
+
+  useEffect(() => {
+    if (state.errors.length > 0) {
+      onClose();
+
+      Swal.fire({
+        icon: "error",
+        title: "訊息發送失敗",
+        text: "您的訊息無法順利寄出，請重新發送!!",
+      });
+    }
+  }, [state.errors]);
+
   return (
-    <div>
+    <FormContainer>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Email Address</label>
-        <input id="email" type="email" name="email" />
-        <ValidationError prefix="Email" field="email" errors={state.errors} />
-        <textarea id="message" name="message" />
-        <ValidationError
-          prefix="Message"
-          field="message"
-          errors={state.errors}
+        <TextField
+          autoFocus
+          margin="dense"
+          id="email"
+          label="Email"
+          name="email"
+          type="email"
+          fullWidth
+          placeholder="請輸入Email"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <EmailIcon />
+              </InputAdornment>
+            ),
+          }}
+          sx={{ m: "8px 0px" }}
+          focused
         />
-        <button type="submit" disabled={state.submitting}>
-          Submit
-        </button>
+        <TextField
+          id="message"
+          label="Message"
+          name="message"
+          placeholder="請輸入內容"
+          multiline
+          fullWidth
+          rows={5}
+          sx={{ m: "8px 0px" }}
+          focused
+        />
+        <Button
+          variant="contained"
+          type="submit"
+          sx={{ m: "8px 0px", width: 100, float: "right" }}
+        >
+          送出
+        </Button>
       </form>
-    </div>
+    </FormContainer>
   );
 }
 
